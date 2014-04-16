28046ecd98e8496cc2d8f0242d7ecd413abf219f
Daniel Kocsan <dani@kocsan.hu>
2014-04-16 22:51:32 +0200

    set, get, subscribe methods added

--- specs/liveState.spec.js
+++ specs/liveState.spec.js
@@ -9,6 +9,7 @@ describe('LiveState object', function () {
 
     beforeEach(function () {
         SUT = dio.di.getCustomInstance('State');
+        SUT.reset();
     });
 
     describe('SHOULD has an interface which', function () {
@@ -21,16 +22,16 @@ describe('LiveState object', function () {
     });
 
     describe("GIVEN an element's value is set", function () {
-        var mockProperty = 'a',
+        var mockPath = 'a',
             mockValue = 'test';
 
         beforeEach(function () {
-            SUT.set(mockProperty, mockValue);
+            SUT.set(mockPath, mockValue);
         });
 
-        describe("WHEN get method has called with the elementName only THEN", function () {
-            it('returns the set value', function () {
-                expect(SUT.get(mockProperty)).toEqual(mockValue);
+        describe("WHEN get method has called with the elementName only", function () {
+            it('THEN it returns the set value', function () {
+                expect(SUT.get(mockPath)).toEqual(mockValue);
             });
         });
 
@@ -50,11 +51,105 @@ describe('LiveState object', function () {
 
             describe("WHEN get method has called with the parent element's name only THEN", function () {
                 it('returns the set value', function () {
-                    expect(SUT.get(mockProperty)).toEqual(mockValue);
+                    expect(SUT.get(mockPath)).toEqual(mockValue);
                 });
+            });
+        });
+        
+        describe("WHEN reset method has called", function () {
+            beforeEach(function () {
+                SUT.reset();
+            });
+            
+            it('THEN it returns the set value', function () {
+                expect(SUT.get(mockPath)).toBeUndefined();
+            });
+        });
+    });
+    
+    
+    describe("GIVEN an element's attributes are set", function () {
+        var mockPath = 'a',
+            mockAttributes = {
+                alpha: 'alpha',
+                bravo: 'bravo',
+                charlie: 'charlie'
+            };
+
+        beforeEach(function () {
+            SUT.set(mockPath, mockAttributes.alpha, 'alpha');
+            SUT.set(mockPath, mockAttributes.bravo, 'bravo');
+            SUT.set(mockPath, mockAttributes.charlie, 'charlie');
+        });
+        
+        describe("WHEN get method has called with the elementName and the attribure name", function () {
+            it('THEN it returns the set value', function () {
+                expect(SUT.get(mockPath, 'alpha')).toEqual(mockAttributes.alpha);
+                expect(SUT.get(mockPath, 'bravo')).toEqual(mockAttributes.bravo);
+                expect(SUT.get(mockPath, 'charlie')).toEqual(mockAttributes.charlie);
+            });
+        });
+        
+        describe("WHEN get method has called with the elementName only", function () {
+            it('THEN it returns undefined', function () {
+                expect(SUT.get(mockPath)).toBeUndefined();
+            });
+        });
+
+        describe("WHEN get method has called with the elementName and '*'", function () {
+            it('THEN it returns the attributes object', function () {
+                expect(SUT.get(mockPath, '*')).toEqual(mockAttributes);
             });
         });
     });
 
+    describe("GIVEN there is a subscription to 'change' on an element", function () {
+        var handlerSpy,
+            mockElement = 'b',
+            mockValue = 'test',
+            mockAttributes = {
+                alpha: 'alpha'
+            };
+        
+        beforeEach(function () {
+            handlerSpy = jasmine.createSpy('handlerSpy');
+            SUT.subscribe(mockElement, 'change', handlerSpy);
+        });
+        
+        describe("WHEN the property's value has changed", function () {
 
+            beforeEach(function () {
+                SUT.set(mockElement, mockValue);
+            });
+
+            it('THEN the handler function should be called with proper parameters', function () {
+                expect(handlerSpy).toHaveBeenCalled();
+                expect(handlerSpy.calls.length).toEqual(1);
+                expect(handlerSpy.calls[0].args[0]).toEqual('change');
+                expect(handlerSpy.calls[0].args[1]).toEqual(mockElement);
+                expect(handlerSpy.calls[0].args[2]).toEqual(mockValue);
+                expect(handlerSpy.calls[0].args[3]).toEqual({});
+                expect(handlerSpy.calls[0].args[4]).toEqual(true);
+                expect(handlerSpy.calls[0].args[5]).toBeUndefined();
+            });
+        });
+
+        describe("WHEN the property's any attribute changed", function () {
+
+            beforeEach(function () {
+                SUT.set(mockElement, mockAttributes.alpha, 'alpha');
+            });
+
+            it('THEN the handler function should be called with proper parameters', function () {
+                expect(handlerSpy).toHaveBeenCalled();
+                expect(handlerSpy.calls.length).toEqual(1);
+                expect(handlerSpy.calls[0].args[0]).toEqual('change');
+                expect(handlerSpy.calls[0].args[1]).toEqual(mockElement);
+                expect(handlerSpy.calls[0].args[2]).toBeUndefined();
+                expect(handlerSpy.calls[0].args[3]).toEqual(mockAttributes);
+                expect(handlerSpy.calls[0].args[4]).toEqual(false);
+                expect(handlerSpy.calls[0].args[5]).toEqual('alpha');
+            });
+        });
+    });
 });
--- src/liveState.js
+++ src/liveState.js
@@ -3,30 +3,97 @@
         var state = {};
 
         createElement = function (elementPath) {
-            state[elementPath] = {
-                attrs: {}
-            };
+            if (!state[elementPath]) {
+                state[elementPath] = {
+                    attrs: {},
+                    handlers: {}
+                };
+            }
         };
 
         setElementValue = function (elementPath, value) {
             state[elementPath].value = value;
+            callHandlerFunction(elementPath, 'change', true);
         };
 
         getElementValue = function (elementPath) {
-            return state[elementPath].value;
+            var result; 
+
+            if (state[elementPath] && state[elementPath].value) {
+                result = state[elementPath].value;
+            }
+
+            return result;
+        };
+
+        setElementAttribute = function (elementPath, attributeName, attributeValue) {
+            state[elementPath].attrs[attributeName] = attributeValue;
+            callHandlerFunction(elementPath, 'change', false, attributeName);
+        };
+        
+        getElementAttribute = function (elementPath, attributeName) {
+            var result;
+
+            if (attributeName === '*') {
+                result = state[elementPath].attrs;
+            } else {
+                result = state[elementPath].attrs[attributeName];
+            }
+
+            return result;
+        };
+
+        emptyState = function () {
+            state = {};
+        };
+
+        addHandlerFunction = function (elementPath, eventName, handlerFunction) {
+            state[elementPath].handlers[eventName] = handlerFunction;
+        };
+
+        callHandlerFunction = function (elementPath, eventName, valueChanged, changeAttributeName) {
+            if (state[elementPath].handlers[eventName]) {
+                state[elementPath].handlers[eventName].call(
+                    this,
+                    eventName,
+                    elementPath,
+                    getElementValue(elementPath),
+                    getElementAttribute(elementPath, '*'),
+                    valueChanged,
+                    changeAttributeName
+                );
+            }
         };
 
         return {
-            set: function (elementPath, value) {
+            set: function (elementPath, value, attributeName) {
                 createElement(elementPath);
-                setElementValue(elementPath, value);
+ 
+                if(attributeName) {
+                    setElementAttribute(elementPath, attributeName, value);
+                } else {
+                    setElementValue(elementPath, value);
+                }
             },
-            get: function (elementPath) {
-                return getElementValue(elementPath);
+            get: function (elementPath, attributeName) {
+                var result;
+
+                if (attributeName) {
+                    result = getElementAttribute(elementPath, attributeName);
+                } else {
+                    result = getElementValue(elementPath);
+                }
+ 
+                return result;
             },
             getDOM: function () {},
-            subscribe: function () {},
-            reset: function () {},
+            subscribe: function (elementPath, eventName, handlerFunction) {
+                createElement(elementPath);
+                addHandlerFunction(elementPath, eventName, handlerFunction);
+            },
+            reset: function () {
+                emptyState();
+            },
             bind: function () {}
         };
     };
